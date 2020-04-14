2742bced635cf205685fcb07755c5dbe3a355d02
lcxzjdfvodo <55543101+lcxzjdfvodo@users.noreply.github.com>
2020-04-14 14:40:28 +0800

    Add files via upload

---
+++ BinarySearch.java
@@ -0,0 +1,32 @@
+package com.arrays;
+
+
+    import java.util.Arrays;
+    public class BinarySearch {
+        public static void main(String[] args) {
+            int[] arr = { 30,20,50,10,80,9,7,12,100,40,8};
+            int searchWord = 80; // 所要查找的数
+            Arrays.sort(arr); //二分法查找之前，一定要对数组元素排序
+            System.out.println(Arrays.toString(arr));
+            System.out.println(searchWord+"元素的索引："+binarySearch(arr,searchWord));
+        }
+
+        public static int binarySearch(int[] array, int value){
+            int low = 0;
+            int high = array.length - 1;
+            while(low <= high){
+                int middle = (low + high) / 2;
+                if(value == array[middle]){
+                    return middle;         //返回查询到的索引位置
+                }
+                if(value > array[middle]){
+                    low = middle + 1;
+                }
+                if(value < array[middle]){
+                    high = middle - 1;
+                }
+            }
+            return -1;     //上面循环完毕，说明未找到，返回-1
+        }
+    }
+
