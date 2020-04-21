c827b3f1cd5d86051770226dd427799468612e5e
lcxzjdfvodo <55543101+lcxzjdfvodo@users.noreply.github.com>
2020-04-21 13:12:27 +0800

    Add files via upload

---
+++ Test01.java
@@ -0,0 +1,24 @@
+package com.arrays;
+
+public class Test01 {
+    public static void main(String[] args) {
+        int[] arr01 = new int[10];
+        String arr02[] = new String[5];
+        int i;
+        for (i = 0; i < arr01.length; i++) {
+
+        arr01[0] = 10;
+        arr01[1] = 15;
+        arr01[2] = 20;
+        arr01[3] = 25;
+        arr01[4] = 30;
+        arr01[5] = 35;
+        arr01[6] = 40;
+        arr01[7] = 45;
+        arr01[8] = 50;
+        arr01[9] = 55;
+
+        System.out.println(arr01[i]);
+        }
+    }
+}
