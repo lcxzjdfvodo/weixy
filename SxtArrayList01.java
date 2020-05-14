fb0bd1672a7af05c7b92abb5349a7002d2d5a00b
lcxzjdfvodo <55543101+lcxzjdfvodo@users.noreply.github.com>
2020-05-14 16:04:18 +0800

    Add files via upload

---
+++ SxtArrayList01.java
@@ -0,0 +1,48 @@
+package com.mycollection;
+
+import java.util.Queue;
+
+/*自定义实现一个Arraylist，体会底层原理
+
+ */
+public class SxtArrayList01 {
+    private Object[] elementData;
+    private int size;
+
+    private static final int DEFALT_CAPACITY=10;
+    public SxtArrayList01() {
+        elementData = new Object[DEFALT_CAPACITY];
+    }
+
+    public SxtArrayList01(int capacity) {
+        elementData = new Object[capacity];
+    }
+
+    public void add(Object obj) {
+        elementData[size++] = obj;
+    }
+
+    public String toString(){
+        StringBuilder sb=new StringBuilder();
+
+        //[a,b,c]
+        sb.append("[");
+        for(int i=0;i<size;i++) {
+            sb.append(elementData[i] + "~");
+        }
+        sb.setCharAt(sb.length()-1,']');
+
+        return sb.toString();
+        }
+
+    public static void main(String[] args) {
+        SxtArrayList01 s1=new SxtArrayList01(20);
+        s1.add("aa");
+        s1.add("bb");
+
+        System.out.println(s1);
+    }
+    }
+
+
+
