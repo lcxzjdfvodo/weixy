fbf0c5638fe943189276c4d7ef0d1b8d161887b4
lcxzjdfvodo <55543101+lcxzjdfvodo@users.noreply.github.com>
2020-07-04 21:38:39 +0800

    Add files via upload

---
+++ qiantao.java
@@ -0,0 +1,41 @@
+public class qiantao {
+    public static void main(String[] args) {
+        /*for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                System.out.print(i+"\t");
+            }
+            System.out.println();
+            }
+        System.out.println("###########");
+
+        for(int n=1;n<=9;n++){
+            for(int m=1;m<=n;m++){
+                System.out.print(m+"*"+n+"="+(m*n)+"\t");
+            }
+            System.out.println();
+        }
+        System.out.println("################");
+        //计算100以内奇数，偶数的和，并输出
+        int sum1=0;
+        int sum2=0;
+        for(int i=1;i<=100;i++){
+            if(i%2==0) {//偶数
+                sum1 += i;//sum1=sum1+i
+            }else {
+                sum2 +=i;
+            }
+        }
+        System.out.println("奇数和："+sum1);
+        System.out.println("偶数和"+sum2);*/
+
+        for(int i=1;i<=1000;i++){
+            if(i%5==0){
+                System.out.print(i+"\t");
+            }
+            if(i%25==0){
+                System.out.println();
+            }
+        }
+        }
+    }
+
